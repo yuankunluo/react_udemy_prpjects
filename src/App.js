ad3a89925052a51e62c799f15d0c3044fdd50261
yuankunluo <yuankunluo@outlook.com>
2020-03-18 21:26:43 +0800

    prepare to do Monster

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>My name is Yihua</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
