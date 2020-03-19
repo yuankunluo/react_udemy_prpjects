a818fa379783adaa334c523cdaf7cc488c5b08f9
yuankunluo <yuankunluo@outlook.com>
2020-03-19 19:08:19 +0800

    add router to MenuItem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
-     
+      {/* <HomePage></HomePage> */}
+
+      <Switch>
+        <Route exact={true} path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+
     </div>
   );
 }
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 export default App;
