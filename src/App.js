1db13653850fc042b650e0ca261697e566447689
yuankunluo <yuankunluo@outlook.com>
2020-03-20 00:31:18 +0800

    sign in com ok, start to do firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop-page.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
 
     </div>
