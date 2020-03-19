ac06070e37a6abb9bbf417b8556c764ebdd85887
yuankunluo <yuankunluo@outlook.com>
2020-03-19 10:29:24 +0800

    filteredMonster ok

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
     this.state = {
       monsters: [
         
-      ]
+      ],
+      searchField: ''
     }
   }
 
@@ -21,12 +22,16 @@ class App extends Component {
     .then( users => this.setState({monsters: users}) );
   }
 
-  render(){
+  render(){ 
+      // Pulling out states.
+      const { monsters, searchField} = this.state;
+      const filteredMonsters = monsters.filter( 
+        monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
       return (
-
         <div className="App">
-          <CardList monsters= {this.state.monsters}/>
-       
+          <input type="search" placeholder="search monster" 
+          onChange={e => this.setState({searchField: e.target.value})} />
+          <CardList monsters= {filteredMonsters}/>
         </div>
       );
     
