2bb55982556143d8ff073194d36ad49987b52a46
yuankunluo <yuankunluo@outlook.com>
2020-03-20 17:04:21 +0800

    split UserActionType

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 // Redux.
 import { connect } from 'react-redux';
@@ -49,16 +49,20 @@ class App extends React.Component {
         <Switch>
           <Route exact={true} path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUp} />
+          <Route exact={true} path='/signin' render={()=> this.props.currentUser ?  (<Redirect to="/" />): (<SignInAndSignUp/>)}/>
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 // We don't need state for
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
